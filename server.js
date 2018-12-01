c67989d33d3621f6a09517a55087acc1061bf040
Samuel <atulesamuel20@gmail.com>
2018-12-01 13:34:37 +0000

    editted dashboard

--- server.js
+++ server.js
@@ -84,11 +84,13 @@ app.post('/upload', fileParser, function(req, res) {
                 console.error("Error occured " + err);
             });
 
+            res.sendFile(path.join(__dirname, '/public/html/dashboard.html'));
+
             //res.send({url: result.url}); //Save this url to a database instead of outputing it
-            res.status(200).json({
-                message: "File uploaded successfully",
-                url: result.url
-            });
+            // res.status(200).json({
+            //     message: "File uploaded successfully",
+            //     url: result.url
+            // });
 
         } else {
             /*
